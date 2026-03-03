23f1cb7d383660b80120dfd69a3115fa93fa020b
Nishal Pattan <nishal-pattan@gmail.com>
2026-03-03 01:33:48 -0600

    Add Trees solutions 2026-03-03

---
+++ NeetCode-250/Trees/same_binary_tree.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# https://neetcode.io/problems/same-binary-tree/question
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        def preorder(p, q):
+            # base case
+            if p is None and q is None: return True
+            # edge case
+            if (p and not q) or (not p and q):
+                return False
+            if p.val != q.val:
+                return False
+
+            left_subtree = preorder(p.left, q.left)
+            right_subtree = preorder(p.right, q.right)
+            return left_subtree and right_subtree
+
+        return preorder(p, q)
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+
